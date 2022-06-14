ad6e281fc19e31857f4f2d03c69bfdc518267328
Martin Braun <braun@neuroforge.de>
2022-06-14 23:09:32 +0200

    add alertmanager setup

--- environments/test/test-swarm/stacks/02_monitoring/secrets/vars.sh
+++ environments/test/test-swarm/stacks/02_monitoring/secrets/vars.sh
@@ -3,3 +3,5 @@
 # this gets prefixed with <servicename>- to get the domain
 export BASE_DOMAIN="mon.test.tld.localhost"
 export PROMETHEUS_USERS=$(bash prometheus_users.sh)
+export ALERTMANAGER_USERS=$(bash alertmanager_users.sh)
+export PROM2TEAMS_CONNECTOR="<MS Teams webhook url>"
