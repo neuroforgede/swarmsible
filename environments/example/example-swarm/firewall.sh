abcc78da853636ff7e87a4092750b8fb973c41e9
Martin Braun <braun@neuroforge.de>
2022-10-03 12:57:07 +0200

    add firewall rules to make caddy exporter for dockerd work

---
+++ environments/example/example-swarm/firewall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+check_result () {
+    ___RESULT=$?
+    if [ $___RESULT -ne 0 ]; then
+        echo $1
+        exit 1
+    fi
+}
+
+ansible-playbook -i inventory/hosts.yml ../../../swarmsible/docker_swarm_firewall.yml
+check_result "failed to run docker_swarm_volumes"
